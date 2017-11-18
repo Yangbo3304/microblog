3618877f899c1f17aaa3bb5a534cc133707c86de
Bob Yang <2739296373@qq.com>
2017-11-18 16:02:18 +0800

    add templates

--- application/views.py
+++ application/views.py
@@ -1,8 +1,22 @@
 # -*- coding: utf-8 -*-
+from flask import render_template
+
 from application import app
 
 
 @app.route('/', methods=['GET'])
 @app.route('/index', methods=['GET'])
 def index():
-    return "Hello...micro blog!"
+    user = {'nickname': 'Miguel'}  # 伪造的用户
+    # 伪造的帖子数组
+    posts = [
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template("index.html", title='Home', user=user, posts=posts)
