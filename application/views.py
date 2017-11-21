08b04d897b83c01312149db258113d2058375b45
Bob Yang <2739296373@qq.com>
2017-11-21 08:18:02 +0800

    update2 User information page

--- application/views.py
+++ application/views.py
@@ -11,7 +11,7 @@ from datetime import datetime
 @app.before_request
 def before_request():
     g.user = current_user
-    if g.user.is_authenticated():
+    if g.user.is_authenticated:
         g.user.last_seen = datetime.utcnow()
         db.session.add(g.user)
         db.session.commit()
